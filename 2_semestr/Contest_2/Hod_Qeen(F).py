620bd15077d2d8892a45c64889b257de8bff7983
Idrisov Sergey <idrisovsergei@phystech.edu>
2024-04-28 15:30:43 +0300

    f

--- 2_semestr/Contest_2/Hod_Qeen(F).py
+++ 2_semestr/Contest_2/Hod_Qeen(F).py
@@ -5,3 +5,53 @@
 Алгоритм Уна или потоки
 Найти максимально паросочетание
 """
+MAX = 301
+
+g = [[0]*MAX for _ in range(MAX)]
+used = []
+par = []
+mt = []
+rows, cols, cuts, a, b, flow = 0, 0, 0, 0, 0, 0
+
+def dfs(v):
+    if used[v]:
+        return 0
+    used[v] = 1
+    for to in range(cols):
+        if g[v][to] == 0:
+            continue
+        if mt[to] == -1 or dfs(mt[to]):
+            mt[to] = v
+            par[v] = 1
+            return 1
+    return 0
+
+def AugmentingPath():
+    global rows, cols
+    run = 1
+    global mt, par, used
+    mt = [-1]*cols
+    par = [-1]*rows
+    while run:
+        run = 0
+        used = [0]*rows
+        for i in range(rows):
+            if par[i] == -1 and dfs(i):
+                run = 1
+num = int(input())
+for o in range(num):
+    vvod = list(map(int, input().split()))
+    if vvod != []:
+        rows, cols, cuts = vvod[0], vvod[1], vvod[2]
+        o-=1
+        for i in range(rows):
+            for j in range(cols):
+                g[i][j] = 1
+        c = list(map(int, input().split()))
+        for j in range(cuts):
+            g[c[2*j]][c[2*j+1]] = 0
+        AugmentingPath()
+        flow = sum(1 for i in range(cols) if mt[i] != -1)
+        print(flow)
+    else:
+        continue
